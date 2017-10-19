2969f641ce25ccf86d49bd391b344d31712a95f1
Martoon <martoon.hsk@gmail.com>
2017-10-19 13:26:22 +0300

    [CSM-500] Remember pending transaction from the very beginning
    This is important, because if we remember it once transaction creation is done,
    then creation may complete only after the moment when transaction gets to
    blocks and then gets detected by tracker, so effectively tracker misses the tx.
    
    Solution is to add new pending transaction status, `PtxCreating`. which
    is not to display to frontend, and such transaction may be removed if
    full cycle of transaction creation finishes with fatal error (unlike
    `PtxApplying`).
    Transaction remembered with this status still caries all pending meta info,
    so tracker can successfully update status of that transaction.
    
    (cherry picked from commit 2c1cfa3290af33e4b34f6f6ebfe46315af1ce2cb)

--- wallet/src/Pos/Wallet/Web/Pending/Submission.hs
+++ wallet/src/Pos/Wallet/Web/Pending/Submission.hs
@@ -13,7 +13,7 @@ module Pos.Wallet.Web.Pending.Submission
 
 import           Universum
 
-import           Control.Monad.Catch          (Handler (..), catches)
+import           Control.Monad.Catch          (Handler (..), catches, onException)
 import           Formatting                   (build, sformat, shown, stext, (%))
 import           System.Wlog                  (WithLogger, logDebug, logInfo, logWarning)
 import           Serokell.Util                (hour)
@@ -22,14 +22,16 @@ import           Pos.Client.Txp.History       (saveTx, thTimestamp)
 import           Pos.Communication            (EnqueueMsg, submitTxRaw)
 import           Pos.Configuration            (walletTxCreationDisabled)
 import           Pos.Core                     (getCurrentTimestamp, diffTimestamp)
+import           Pos.Util.Util                (maybeThrow)
 import           Pos.Wallet.Web.Error         (WalletError (..))
 import           Pos.Wallet.Web.Mode          (MonadWalletWebMode)
 import           Pos.Wallet.Web.Pending.Types (PendingTx (..), PtxCondition (..),
                                                PtxPoolInfo)
-import           Pos.Wallet.Web.Pending.Util  (isReclaimableFailure)
+import           Pos.Wallet.Web.Pending.Util  (isReclaimableFailure, ptxPoolInfo,
+                                               usingPtxCoords)
 import           Pos.Wallet.Web.State         (PtxMetaUpdate (PtxMarkAcknowledged),
                                                addOnlyNewPendingTx, casPtxCondition,
-                                               ptxUpdateMeta)
+                                               ptxUpdateMeta, removeOnlyCreatingPtx)
 
 -- | Handers used for to procees various pending transaction submission
 -- errors.
@@ -129,10 +131,15 @@ submitAndSavePtx PtxSubmissionHandlers{..} enqueue ptx@PendingTx{..} = do
                       \the 1h time limit was exceeded")
                       _ptxTxId
        | otherwise -> do
+           addOnlyNewPendingTx ptx
            ack <- submitTxRaw enqueue _ptxTxAux
            reportSubmitted ack
-           saveTx (_ptxTxId, _ptxTxAux) `catches` handlers ack
-           addOnlyNewPendingTx ptx
+           (saveTx (_ptxTxId, _ptxTxAux)
+               `catches` handlers ack)
+               `onException` creationFailedHandler
+
+           poolInfo <- badInitPtxCondition `maybeThrow` ptxPoolInfo _ptxCond
+           _ <- usingPtxCoords casPtxCondition ptx _ptxCond (PtxApplying poolInfo)
            when ack $ ptxUpdateMeta _ptxWallet _ptxTxId PtxMarkAcknowledged
   where
     handlers accepted =
@@ -146,14 +153,12 @@ submitAndSavePtx PtxSubmissionHandlers{..} enqueue ptx@PendingTx{..} = do
             -- but it's better to try with tx again than to regret, right?
             minorError "unknown error" e
         ]
-
     minorError desc e = do
         reportError desc e ", but was given another chance"
         pshOnMinor e
     nonReclaimableError accepted e = do
         reportError "fatal" e ""
         pshOnNonReclaimable accepted e
-
     reportError desc e outcome =
         logInfo $
         sformat ("Transaction #"%build%" application failed ("%shown%" - "
@@ -163,3 +168,10 @@ submitAndSavePtx PtxSubmissionHandlers{..} enqueue ptx@PendingTx{..} = do
         sformat ("submitAndSavePtx: transaction submitted with confirmation?: "
                 %build) ack
 
+    creationFailedHandler =
+        -- tx creation shouldn't fail if any of peers accepted our tx, but still,
+        -- if transaction was detected in blocks and its state got updated by tracker
+        -- while transaction creation failed, due to protocol error or bug,
+        -- then we better not remove this pending transaction
+        void $ usingPtxCoords removeOnlyCreatingPtx ptx
+    badInitPtxCondition = InternalError "Expected PtxCreating as initial pending condition"
